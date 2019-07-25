924535feb6761774ecdf6ffb6c10fcfa1945bbca
student <student@MacBook-Pro-2.local>
2019-07-25 10:53:36 -0400

    stuff

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import webapp2
 import jinja2
 import os
-
+from models import User
 # this initializes the jinja2 environment
 # this will be the same in every app that uses the jinja2 templating library 
 the_jinja_env = jinja2.Environment(
@@ -17,19 +17,37 @@ class MainHandler(webapp2.RequestHandler):
   def get(self):  # for a get request
     start_template = the_jinja_env.get_template('templates/index.html')  # path to index.html
     self.response.write(start_template.render()) # render index.html
+  def post(self):
+    username=self.request.get('username')
+    password=self.request.get('password')     
+    user=User(username=username)
+    user.put()
+ 
+    start_template = the_jinja_env.get_template('templates/index.html')
+    self.response.write(start_template.render())    
 
 class GameHandler(webapp2.RequestHandler):
   def get(self):
 	game_template = the_jinja_env.get_template('templates/game-start.html') # path to game-start.html
 	self.response.write(game_template.render()) # render game-start.html
 
- 
-
+class ScoreHandler(webapp2.RequestHandler):
+   def get(self):
+    score_template = the_jinja_env.get_template('templates/scoreboard.html')
+    self.response.write(score_template.render())
+        
+class SignupHandler(webapp2.RequestHandler):
+  def get(self):
+    sign_template = the_jinja_env.get_template('templates/signup.html')
+    self.response.write(sign_template.render())
+    
 # the app configuration section	
 app = webapp2.WSGIApplication([
   #('/', MainPage),
   ('/', MainHandler),
-  ('/game', GameHandler)
+  ('/game', GameHandler),
+  ('/score', ScoreHandler),
+  ('/signup',SignupHandler)
   ], debug=True)
 
 
